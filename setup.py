463a8645713b900ec1965a4e011e67751d2f3461
Evan Sultanik <evan@sultanik.com>
2021-09-10 13:03:06 -0400

    Up the version number

--- setup.py
+++ setup.py
@@ -5,10 +5,13 @@ setup(
     description='Visie is a simple initialism enumerator. It helps you name things.',
     url='https://github.com/ESultanik/visie',
     author='Evan Sultanik',
-    version='0.1.0',
+    version='0.1.1',
     packages=find_packages(),
     python_requires='>=3.6',
     install_requires=[],
+    extras_require={
+        "dev": ["flake8", "pytest", "twine", "mypy>=0.812"]
+    },
     entry_points={
         'console_scripts': [
             'visie = visie.__main__:main'
